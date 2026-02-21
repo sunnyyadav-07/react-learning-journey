936d643f41db3855fad9beb62e33d437a4e14f8f
Sunny Yadav <yadavsunny1916@gmail.com>
2026-02-21 17:54:45 +0530

    built:initiate macOS project with setup and styling

---
+++ day38/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './app.scss'
+import Dock from './components/Dock'
+import Navbar from './components/Navbar'
+function App() {
+
+  return (
+<>
+
+<main>
+  <Navbar/>
+  <Dock/>
+  </main></>
+  )
+}
+
+export default App
---
+++ day38/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './nav.scss'
+const Navbar = () => {
+  return (
+    <nav>
+      
+    </nav>
+  )
+}
+
+export default Navbar
